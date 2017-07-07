89b7b8010ce95d7f2ff7d3db32ee26bdaf5868f7
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-07 10:02:15 +0200

    @

---
+++ options/@/main.c
@@ -0,0 +1,15 @@
+//
+//   Put the useful flags into a file and compile
+//   with the @file-name option.
+//
+//   gcc main.c @compiler-options
+//
+
+#include <stdio.h>
+int main() {
+
+  int x;
+
+  printf("x = %d\n", x);
+
+}
