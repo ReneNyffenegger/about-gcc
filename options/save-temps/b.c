99ea1d14e77e97fd5142986ba612aad08f3795f5
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-07 09:10:26 +0200

    -save-temps

---
+++ options/save-temps/b.c
@@ -0,0 +1,10 @@
+#include "b.h"
+
+void b1() {
+  printf("b1\n");
+}
+
+void b2() {
+  printf("b2\n");
+}
+
---
+++ options/save-temps/main.c
@@ -0,0 +1,15 @@
+//
+//  gcc a.c b.c main.c -o save-temps
+//
+
+#include "a.h"
+#include "b.h"
+
+int main() {
+
+  a1();
+  b1();
+  a2();
+  b2();
+
+}
