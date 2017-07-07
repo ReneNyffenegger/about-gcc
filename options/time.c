ae3d9632c052d66483c0fbde586b30a3a8905386
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-07 09:12:01 +0200

    + time.c

---
+++ options/time.c
@@ -0,0 +1,12 @@
+//
+//  -time:
+//    Report the CPU time taken by each subprocess in the compilation sequence.
+//
+//  gcc -time time.c
+//
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello world\n");
+}
