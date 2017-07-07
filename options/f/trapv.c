85bdab305d6f39f19390acffd55784217829ec70
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-07 09:26:53 +0200

    -ftrapv seems to be broken

--- options/f/trapv.c
+++ options/f/trapv.c
@@ -1,5 +1,9 @@
 //
 //  gcc -ftrapv trapv.c
+//
+//    Apparently, there's a bug with -ftrapv:
+//      https://gcc.gnu.org/bugzilla/show_bug.cgi?id=35412
+//      https://stackoverflow.com/questions/20851061/how-to-make-gcc-ftrapv-work
 
 #include <limits.h>
 #include <stdio.h>
