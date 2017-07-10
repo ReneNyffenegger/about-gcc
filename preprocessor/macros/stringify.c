75d994ecf9365ad1d80b16e94263d68fbc4550cb
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-10 23:55:03 +0200

    + stringify.c

---
+++ preprocessor/macros/stringify.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//   QuoteIdent turns an unquoted identifier
+//   into a (quoted) string:
+//      QuoteIdent(foo) -> "foo"
+#define QuoteIdent(ident) #ident
+
+//   QuoteMacro can be used to turn the value
+//  (rather then the name/ident of the macro)
+//   into a string
+#define QuoteMacro(macro) QuoteIdent(macro)
+
+
+#define FRUIT   strawberry
+#define I_LIKE  FRUIT
+
+int main() {
+  printf("QuoteIdent(FRUIT)  = %s\n", QuoteIdent(FRUIT) );
+  printf("QuoteMacro(FRUIT)  = %s\n", QuoteMacro(FRUIT) );
+  printf("QuoteMacro(I_LIKE) = %s\n", QuoteMacro(I_LIKE));
+}
