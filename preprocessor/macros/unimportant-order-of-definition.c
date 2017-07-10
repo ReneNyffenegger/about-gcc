abea6bcdd538b9adee054b3df769c91626d63d6a
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-10 23:37:23 +0200

    + unimportant-order-of-definition.c

---
+++ preprocessor/macros/unimportant-order-of-definition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define NUMBER         FORTY_TWO
+#define FORTY_TWO             42
+
+#define FIVE                   5
+#define ANOTHER_NUMBER      FIVE
+
+
+int main() {
+
+  printf("NUMBER         is %2d\n",         NUMBER);
+  printf("ANOTHER_NUMBER is %2d\n", ANOTHER_NUMBER);
+
+}
