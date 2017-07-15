2efd27cd34f65c3bf38363ecef35d36c3fc5f34c
René Nyffenegger <mail@renenyffenegger.ch>
2017-07-15 06:38:11 +0200

    predefined macro __COUNTER__

---
+++ preprocessor/macros/predefined/__COUNTER___/main.c
@@ -0,0 +1,7 @@
+#include "variables.h"
+#include <stdio.h>
+
+int main() {
+  printf("__COUNTER__=%d\n", __COUNTER__);
+  printf("a=%d, b=%d, c=%d\n", a, b, c);
+}
---
+++ preprocessor/macros/predefined/__COUNTER___/variables.h
@@ -0,0 +1,2 @@
+int a = __COUNTER__;
+int b = __COUNTER__; int c = __COUNTER__;
