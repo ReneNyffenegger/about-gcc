f9a2b95be955527a12be42891dabb9fe72f04b82
René Nyffenegger <mail@renenyffenegger.ch>
2018-02-15 11:07:46 +0100

    + invoke_gcc_with_-v

--- options/v/main.c
+++ options/v/main.c
@@ -1,11 +1,5 @@
-//
-//   -v:
-//      Print the commands that were executed during the stages of compilation.
-//
-//   gcc -v main.c
-//
 #include <stdio.h>
 
 int main() {
-  pritntf("Hello world\n");
+  printf("Hello world\n");
 }
